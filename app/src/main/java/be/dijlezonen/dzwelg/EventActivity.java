e8493a1d13a4a2444f9d9d25cf94a5b24f50f178
Wim Hambrouck <wim.hambrouck@gmail.com>
2016-12-09 23:12:22 +0100

    titel eventactivity

--- app/src/main/java/be/dijlezonen/dzwelg/EventActivity.java
+++ app/src/main/java/be/dijlezonen/dzwelg/EventActivity.java
@@ -2,9 +2,6 @@ package be.dijlezonen.dzwelg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.GridView;
-
-import butterknife.ButterKnife;
 
 @java.lang.SuppressWarnings("squid:MaximumInheritanceDepth")
 public class EventActivity extends AppCompatActivity {
@@ -13,6 +10,8 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        setTitle(getString(R.string.activity_event_titel));
 
         //GridView eventsGrid = ButterKnife.findById(this, R.id.gridEvents);
 
