f78790d250b0b188cd45398d329d97faef753146
Wim Hambrouck <wim.hambrouck@gmail.com>
2016-12-21 18:33:35 +0100

    ProgressDialog bij laden van Events

--- app/src/main/java/be/dijlezonen/dzwelg/EventListActivity.java
+++ app/src/main/java/be/dijlezonen/dzwelg/EventListActivity.java
@@ -1,5 +1,6 @@
 package be.dijlezonen.dzwelg;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,17 +22,22 @@ public class EventListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
 
+        final ProgressDialog p = ProgressDialog.show(this, getString(R.string.even_wachten), getString(R.string.laden_van_evenementen), true, false);
+
         setTitle(getString(R.string.activity_event_titel));
 
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference(getString(R.string.ref_evenementen));
-
 
         GridView eventsGrid = ButterKnife.findById(this, R.id.gridEvents);
 
         FirebaseListAdapter<Evenement> fbList = new FirebaseListAdapter<Evenement>(
                 this, Evenement.class, R.layout.event, dbRef) {
+
             @Override
             protected void populateView(View v, final Evenement model, int position) {
+                if (p.isShowing()) {
+                    p.hide();
+                }
                 TextView txtTitel = ButterKnife.findById(v, R.id.txt_event_titel);
                 txtTitel.setText(model.getTitel());
                 v.setOnClickListener(new View.OnClickListener() {
