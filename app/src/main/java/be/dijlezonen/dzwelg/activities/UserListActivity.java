27016a1d41c49d87d71b1d76afbb94045285b544
Wim Hambrouck <wim.hambrouck@gmail.com>
2017-03-19 19:31:35 +0100

    merge

--- app/src/main/java/be/dijlezonen/dzwelg/activities/UserListActivity.java
+++ app/src/main/java/be/dijlezonen/dzwelg/activities/UserListActivity.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
